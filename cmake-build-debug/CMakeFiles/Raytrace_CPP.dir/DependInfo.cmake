764528e9533c53820377329981a645c02c18143a
Morimoto Keigo <keigo7624@gmail.com>
2018-09-21 13:13:31 +0900

    firstCommit

---
+++ cmake-build-debug/CMakeFiles/Raytrace_CPP.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/keigo/CLionProjects/Raytrace_CPP/main.cpp" "/Users/keigo/CLionProjects/Raytrace_CPP/cmake-build-debug/CMakeFiles/Raytrace_CPP.dir/main.cpp.o"
+  "/Users/keigo/CLionProjects/Raytrace_CPP/rayt.cpp" "/Users/keigo/CLionProjects/Raytrace_CPP/cmake-build-debug/CMakeFiles/Raytrace_CPP.dir/rayt.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././lib/stb-master"
+  ".././lib/sce_vectormath-master"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rayt.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Morimoto Keigo on 2018/09/19.
+//
+
